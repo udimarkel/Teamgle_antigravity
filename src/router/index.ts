0c7dab6180ee33ac40e8398c9dfc7753befc0485
udimarkel <udimarkel4@gmail.com>
2026-01-03 18:20:47 +0200

    force hash history for github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 // import { route } from 'quasar/wrappers'
 import {
-    createMemoryHistory,
-    createRouter,
-    createWebHashHistory,
-    createWebHistory,
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
 } from 'vue-router'
 import routes from './routes'
 
@@ -12,18 +12,14 @@ import routes from './routes'
  * directly export the Router instantiation;
  */
 
-const createHistory = import.meta.env.SSR
-    ? createMemoryHistory
-    : createWebHashHistory
-
 const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+  routes,
 
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(import.meta.env.BASE_URL)
+  // ⬇️ זה השינוי החשוב! ⬇️
+  // אנחנו מכריחים אותו לעבוד במצב Hash (עם סולמית #)
+  // ומוחקים את כל התנאים המורכבים.
+  history: createWebHashHistory() 
 })
 
 export default Router
