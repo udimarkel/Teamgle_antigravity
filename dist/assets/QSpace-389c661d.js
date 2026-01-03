a1e73b09ec0dd0fae5b6768f9b15b3cf216a51a1
udimarkel <udimarkel4@gmail.com>
2026-01-03 17:55:36 +0200

    fix: remove node_modules from git

--- dist/assets/QSpace-389c661d.js
+++
@@ -1,1 +0,0 @@
-import{c as a,h as c}from"./index-2e11f86b.js";const p=a({name:"QSpace",setup(){const e=c("div",{class:"q-space"});return()=>e}});export{p as Q};
--- dist/assets/use-quasar-fa299686.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aj as r}from"./index-2e11f86b.js";function u(){return a(r)}export{u};
